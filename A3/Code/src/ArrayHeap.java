5b0eb403b565e153df1507771cdf9b0068c79d11
Hubert <hubalarie@gmail.com>
2018-11-08 21:17:09 -0500

    implemented more methods in ArrayHeap and started JobComparator

--- A3/Code/src/ArrayHeap.java
+++ A3/Code/src/ArrayHeap.java
@@ -43,4 +43,49 @@ public class ArrayHeap<E> {
         }
     }
 
+    public void downheap(int j) {
+        while(hasLeft(j)) {
+            int leftIndex = left(j);
+            int smallChildIndex = leftIndex;
+            if(hasRight(j)) {
+                int rightIndex = right(j);
+                if(compare(heap.get(leftIndex), heap.get(rightIndex)) > 0) {
+                    smallChildIndex = rightIndex;
+                }
+            }
+            if(compare(heap.get(smallChildIndex), heap.get(j)) >= 0) {
+                break;
+            }
+            swap(j, smallChildIndex);
+            j = smallChildIndex;
+        }
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    public E min() {
+        if(heap.isEmpty()) {
+            return null;
+        }
+        return heap.get(0);
+    }
+
+    //TODO finish insert
+    public E insert() {
+        return null;
+    }
+
+    public E removeMin() {
+        if(heap.isEmpty()) {
+            return null;
+        }
+        E answer = heap.get(0);
+        swap(0, heap.size() - 1);
+        heap.remove(heap.size() - 1);
+        downheap(0);
+        return answer;
+    }
+
 }
---
+++ A3/Code/src/JobComparator.java
@@ -0,0 +1,8 @@
+public class JobComparator implements Comparator<Job> {
+
+    //TODO finish compare once job is finished
+    public int compare(Job a, Job b) {
+        return 0;
+    }
+
+}
