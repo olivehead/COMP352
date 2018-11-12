28ad6937d513a950306456ec6dc4043bf4555364
Hubert <hubalarie@gmail.com>
2018-11-11 21:45:13 -0500

    Mostly finished driver for ArrayHeap except starvation

--- A3/Code/src/Main.java
+++ A3/Code/src/Main.java
@@ -1,22 +1,86 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] maxNumberOfJobs = new int[] {100,1000,10000};
-	    Job<Integer,String>[][] jobs = new Job[maxNumberOfJobs.length][maxNumberOfJobs[maxNumberOfJobs.length-1]];
-	    PriorityQueue<Integer,String> pq = new ArrayHeap<>(new JobComparator());
-	    for(int i = 0; i < maxNumberOfJobs.length; i++) {
-	        for(int j = 0; j < maxNumberOfJobs[i]; j++) {
-	            jobs[i][j] = new Job<>();
-	            jobs[i][j].setKey(jobs[i][j].getJobPriority());
-                jobs[i][j].setValue(jobs[i][j].getJobName());
+
+        long startTime;
+        long endTime;
+        long totalTime;
+        long priorityChanges = 0;
+        long averageWaitTime = 0;
+        long cycleCount = 0;
+        int n = 10000;
+
+        PrintWriter fout = null;
+        try {
+            fout = new PrintWriter("out.txt");
+            fout.println();
+            for (int i = 100; i <= n; i *= 10) {
+                Job[] jobs = new Job[i];
+                ArrayHeap<Integer, Job> pq = new ArrayHeap<>(new JobComparator());
+                for (int j = 0; j < jobs.length; j++) {
+                    jobs[j] = new Job();
+                }
+                startTime = System.currentTimeMillis();
+                for (int j = 0; j < i; j++) {
+                    pq.insert(jobs[j]);
+                    jobs[j].setEntryTime(j + 1);
+                }
+                while (!pq.isEmpty()) {
+                    Job j = (Job) pq.removeMin();
+                    j.setCurrentLength(j.getCurrentLength() - 1);
+                    cycleCount += 1;
+                    System.out.println(j);
+                    System.out.println();
+                    if (j.getCurrentLength() != 0) {
+                        j.setLastRun(cycleCount);
+                        pq.insert(j);
+//                    if(cycleCount % 30 == 0) {
+//                        //TODO implement findOldest
+//                        Job oldest = pq.findOldest();
+//                        oldest.setJobPriority(1);
+//                        pq.insert(oldest);
+//                        priorityChanges++;
+//                    }
+                    } else {
+                        j.setEndTime(cycleCount);
+                        j.setWaitTime(j.getEndTime() - j.getJobLength() - j.getEntryTime());
+                        averageWaitTime += j.getWaitTime();
+//                    if(cycleCount % 30 == 0) {
+//                        //TODO implement findOldest
+//                        Job oldest = pq.findOldest();
+//                        oldest.setJobPriority(1);
+//                        pq.insert(oldest);
+//                        priorityChanges++;
+//                    }
+                    }
+                }
+                averageWaitTime /= i;
+                endTime = System.currentTimeMillis();
+                totalTime = endTime - startTime;
+
+                fout.println("Current system time (cycles): " + cycleCount);
+                fout.println("Total number of jobs executed: " + i + " jobs");
+                fout.println("Average process waiting time: " + averageWaitTime + " cycles");
+                fout.println("Total number of priority changes: " + priorityChanges);
+                fout.println("Actual system time needed to execute all jobs: " + totalTime + " ms");
+                fout.println();
+
+                priorityChanges = 0;
+                averageWaitTime = 0;
+                cycleCount = 0;
+
             }
-            Job.resetCounter();
         }
-        for(Job job : jobs[0]) {
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        while(!pq.isEmpty()) {
-            pq.removeMin();
+        finally {
+            if(fout != null) {
+                fout.close();
+            }
         }
     }
 
--- A3/Code/src/Test.java
+++ A3/Code/src/Test.java
@@ -1,13 +1,15 @@
 public class Test {
     //Testing the implementation of ArrayHeap
     public static void main(String[] args) {
+
         long startTime;
         long endTime;
         long totalTime;
         long priorityChanges = 0;
         long averageWaitTime = 0;
         long cycleCount = 0;
-        int n = 10;
+        int n = 100;
+
         ArrayHeap<Integer, Job> pq = new ArrayHeap<>(new JobComparator());
         Job[] jobs = new Job[n];
         for(int i = 0; i < n; i++) {
@@ -16,18 +18,39 @@ public class Test {
         startTime = System.currentTimeMillis();
         for(int i = 0; i < n; i++) {
             pq.insert(jobs[i]);
-            jobs[i].setEntryTime((long) i + 1);
+            jobs[i].setEntryTime(i + 1);
         }
         while(!pq.isEmpty()) {
             Job j = (Job) pq.removeMin();
             j.setCurrentLength(j.getCurrentLength() - 1);
-            cycleCount = cycleCount + 1;
+            cycleCount += 1;
             System.out.println(j);
             System.out.println();
             if(j.getCurrentLength() != 0) {
+                j.setLastRun(cycleCount);
                 pq.insert(j);
+//                if(cycleCount % 30 == 0) {
+//                    //TODO implement findOldest
+//                    Job oldest = pq.findOldest();
+//                    oldest.setJobPriority(1);
+//                    pq.insert(oldest);
+//                    priorityChanges++;
+//                }
+            }
+            else {
+                j.setEndTime(cycleCount);
+                j.setWaitTime(j.getEndTime() - j.getJobLength() - j.getEntryTime());
+                averageWaitTime += j.getWaitTime();
+//                if(cycleCount % 30 == 0) {
+//                    //TODO implement findOldest
+//                    Job oldest = pq.findOldest();
+//                    oldest.setJobPriority(1);
+//                    pq.insert(oldest);
+//                    priorityChanges++;
+//                }
             }
         }
+        averageWaitTime /= n;
         endTime = System.currentTimeMillis();
         totalTime = endTime - startTime;
 
@@ -36,6 +59,7 @@ public class Test {
         System.out.println("Average process waiting time: " + averageWaitTime + " cycles");
         System.out.println("Total number of priority changes: " + priorityChanges);
         System.out.println("Actual system time needed to execute all jobs: " + totalTime + " ms");
+
     }
 
 }
